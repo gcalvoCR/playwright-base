661a8f8f14b1fc8422f983c6a59b7d41723903a2
gcalvoCR <gabriel.calvo.vargas@gmail.com>
2023-02-24 13:38:10 -0600

    antitest added

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -16,3 +16,7 @@ test('get started link', async ({ page }) => {
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
 });
+
+test('Test true is true', async ({ page }) => {
+  await expect(true).toBe(false);
+});
